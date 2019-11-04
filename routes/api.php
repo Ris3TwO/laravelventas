a6a48ab799475faced8d7abbcf0d009e615a7927
Ris3TwO <ris3two@gmail.com>
2019-11-03 22:51:38 -0400

    Configuraciones iniciales

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['domain' => 'api.' . Config::get('app.url')], function () {
+    //
 });
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
